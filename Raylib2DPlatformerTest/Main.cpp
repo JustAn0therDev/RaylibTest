332baf8fe7200bf6263fa250c06a70573c689193
Ruan Montelo <ruanmontelo@gmail.com>
2023-02-03 20:30:37 -0300

    Fixed camera position and ended up writing a camera debugger for it

--- Raylib2DPlatformerTest/Main.cpp
+++ Raylib2DPlatformerTest/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "raylib.h"
 
 Camera3D globalCam{};
@@ -12,6 +13,43 @@ struct entity {
 	Vector3 pos;
 	float speed;
 } typedef Entity;
+
+void WriteDebugCameraInfo() {
+	std::string positionInfo = "positionX: " +
+		std::to_string(globalCam.position.x) +
+		", positionY: " +
+		std::to_string(globalCam.position.y) +
+		", positionZ: " +
+		std::to_string(globalCam.position.z);
+
+	DrawText(positionInfo.c_str(), 2, 0, 20, DARKBROWN);
+
+	std::string targetInfo = "targetX: " +
+		std::to_string(globalCam.target.x) +
+		", targetY: " +
+		std::to_string(globalCam.target.y) +
+		", targetZ: " +
+		std::to_string(globalCam.target.z);
+
+	DrawText(targetInfo.c_str(), 2, 20, 20, DARKBROWN);
+
+	std::string upInfo = "upX: " +
+		std::to_string(globalCam.up.x) +
+		", upY: " +
+		std::to_string(globalCam.up.y) +
+		", upZ: " +
+		std::to_string(globalCam.up.z);
+
+	DrawText(upInfo.c_str(), 2, 40, 20, DARKBROWN);
+
+	std::string fovYInfo = "Fov Y: " + std::to_string(globalCam.fovy);
+
+	DrawText(fovYInfo.c_str(), 2, 60, 20, DARKBROWN);
+
+	std::string projectionInfo = "Projection: " + std::to_string(globalCam.projection);
+
+	DrawText(projectionInfo.c_str(), 2, 80, 20, DARKBROWN);
+}
 
 void Update(Entity& player) {
 	BeginDrawing();
@@ -42,6 +80,8 @@ void Update(Entity& player) {
 		globalCam.position.y += 0.5f;
 	}
 
+	WriteDebugCameraInfo();
+
 	//if ((int)globalCam.position.z == -GLOBAL_CAM_DISTANCE_LIMIT && (int)globalCam.position.y == GLOBAL_CAM_DISTANCE_LIMIT) {
 	//	DrawText("NICE!", (WIDTH / 2) - 100, (HEIGHT / 2) - 40, 80, GREEN);
 	//}
@@ -52,54 +92,55 @@ void Update(Entity& player) {
 }
 
 int main() {
-	InitWindow(WIDTH, HEIGHT, "Title");
+	InitWindow(WIDTH, HEIGHT, "3D walking? IDK");
 
 	SetTargetFPS(60);
 
 	globalCam.position = { 0, 0, 0 };
 	globalCam.target = { 0, 0, 0 };
-	globalCam.up = { 0, 2, 0 };
+	globalCam.up = { 0, 1, 0 };
 	globalCam.fovy = 90;
 	globalCam.projection = CAMERA_PERSPECTIVE;
 
-	Entity player;
-	player.pos = { 0, 2.5f, 0 };
+	Entity player{};
+	player.pos = { 0, 0, 0 };
 	player.speed = 2;
 
 	while (!WindowShouldClose())
 	{
 		Update(player);
 
-		if (IsKeyDown(KEY_R)) {
+		if (IsKeyDown(KEY_R) && SHOULD_FOLLOW_THE_PLAYER_AFTER_ANIMATION) {
+			// RESET DATA
+			globalCam.position.x = 0;
 			globalCam.position.z = 0;
 			globalCam.position.y = 0;
+			player.pos.x = 0;
+			player.pos.y = 0;
+			player.pos.z = 0;
+			globalCam.target = { 0, 0, 0 };
+			SHOULD_FOLLOW_THE_PLAYER_AFTER_ANIMATION = false;
 		}
 
-		if (IsKeyDown(KEY_D)) {
+		if (IsKeyDown(KEY_D) && SHOULD_FOLLOW_THE_PLAYER_AFTER_ANIMATION) {
 			player.pos.x -= player.speed;
 			globalCam.position.x -= player.speed;
 		}
 		
-		if (IsKeyDown(KEY_A)) {
+		if (IsKeyDown(KEY_A) && SHOULD_FOLLOW_THE_PLAYER_AFTER_ANIMATION) {
 			player.pos.x += player.speed;
 			globalCam.position.x += player.speed;
 		}
 		
-		if (IsKeyDown(KEY_W)) {
+		if (IsKeyDown(KEY_W) && SHOULD_FOLLOW_THE_PLAYER_AFTER_ANIMATION) {
 			player.pos.z += player.speed;
 			globalCam.position.z += player.speed;
 		}
 		
-		if (IsKeyDown(KEY_S)) {
+		if (IsKeyDown(KEY_S) && SHOULD_FOLLOW_THE_PLAYER_AFTER_ANIMATION) {
 			player.pos.z -= player.speed;
 			globalCam.position.z -= player.speed;
 		}
-
-		if (IsKeyDown(KEY_B)) {
-			globalCam.up.y += 1;
-		}
-
-		std::cout << globalCam.up.y << std::endl;
 	}
 
 	CloseWindow();
